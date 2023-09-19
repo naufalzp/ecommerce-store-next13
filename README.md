0adb0c806b10e0072192163b249f9f9db2acb4b6
naufalzp <naufalarya0@gmail.com>
2023-09-19 10:33:12 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# NextJS E-Commerce Store
+
+Using api from CMS
+
+### Cloning the repository
+
+```shell
+git clone https://github.com/naufalzp/ecommerce-store-next13.git
+```
+
+### Install packages
+
+```shell
+npm install
+```
+
+### Setup .env file
+
+```js
+NEXT_PUBLIC_API_URL=
+```
+
+### Start the app
+
+```shell
+npm run dev
+```
+
+## Available commands
+
+Running commands with npm `npm run [command]`
+
+| command | description                              |
+| :------ | :--------------------------------------- |
+| `dev`   | Starts a development instance of the app |
---
+++ components/ui/billboard.tsx
@@ -0,0 +1,24 @@
+import { Billboard } from '@/types';
+
+interface BillboardProps {
+  data: Billboard;
+}
+
+const Billboard: React.FC<BillboardProps> = ({ data }) => {
+  return (
+    <div className='overflow-hidden rounded-xl p-4 sm:p-6 lg:p-8'>
+      <div
+        style={{ backgroundImage: `url(${data?.imageUrl})` }}
+        className='relative aspect-square overflow-hidden rounded-xl bg-cover md:aspect-[2.4/1]'
+      >
+        <div className='flex h-full w-full flex-col items-center justify-center gap-y-8 text-center'>
+          <div className='max-w-xs text-3xl font-bold sm:max-w-xl sm:text-5xl lg:text-6xl'>
+            {data?.label || 'No Billboard'}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Billboard;
